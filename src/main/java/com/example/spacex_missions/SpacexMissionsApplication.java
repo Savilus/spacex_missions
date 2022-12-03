c1c888545b10825a55799cf2c6455530839fa3d3
Jakub Łanoszka <jakublanoszka@gmail.com>
2022-12-03 13:53:17 +0100

    spacex

---
+++ src/main/java/com/example/spacex_missions/SpacexMissionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.spacex_missions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacexMissionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpacexMissionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spacex_missions/SpacexMissionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spacex_missions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacexMissionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
